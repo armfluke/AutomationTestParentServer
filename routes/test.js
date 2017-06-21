da30f7cbd0b88ddbf6a39fb6ec18ab46afe7b449
Thanakit Songsirisuk <thanakit.songsirisuk@thomsonreuters.com>
2017-06-21 16:30:43 +0700

    Add remove IP from ips when request is time out

--- routes/test.js
+++ routes/test.js
@@ -6,15 +6,27 @@ var ips = [];
 
 
 router.get('/', function(req,res,next){
-
+    res.send({status: "OK"});
     var c = 0;
     ips = ipgetter.get();
     while(c < ips.length){
-        request('http://'+ips[c].ip+'/test',{json:{name: "EikonLight"}}, function(error, response, body){
-        console.log('error:', error); // Print the error if one occurred 
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-        console.log('body:', body);
-        console.log('***********');
+        var url = 'http://' + ips[c].ip + '/test'
+        console.log(url);
+        request(url ,{json:{name: "EikonLight"}}, function(error, response, body){
+            console.log('error:', error); // Print the error if one occurred
+            if(error){
+                for(let i=0;i<ips.length;i++){
+                    if(ips[i].ip == error.address+':3000'){
+                        ips.pop(i);
+                        break;
+                    }
+                }
+                c--;
+                console.log("Remove "+error.address+" from ips");
+            }
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+            console.log('body:', body);
+            console.log('***********');
     })
     c++
     }  
